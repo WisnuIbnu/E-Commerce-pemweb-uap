b8e106b851c918587519c552c655380b9311aa6b
unknown <abiarkadian12@gmail.com>
2025-11-29 00:45:40 +0700

    membuat 3 akun user(admin, penjual, dan pembeli) serta kategori produk, produk, dan saldo awal toko di DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,123 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Store;
+use App\Models\Buyer;
+use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\ProductImage;
+use App\Models\StoreBalance;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // membuat user admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
+        // membuat user seller
+        $seller = User::create([
+            'name' => 'Noel',
+            'email' => 'noel@gmail.com',
+            'password' => Hash::make('seller'),
+            'role' => 'member', 
+        ]);
+
+        // membuat data toko untuk seller
+        $store = Store::create([
+            'user_id' => $seller->id,
+            'name' => 'Tech Store',
+            'logo' => 'Tech-store.jpg',
+            'about' => 'Toko elektronik terlengkap dan terpercaya.',
+            'phone' => '085895311686',
+            'address_id' => '1',
+            'address' => 'Jl. Veteran No. 50', 
+            'city' => 'Malang',
+            'postal_code' => '121212',
+            'is_verified' => true,
+        ]);
+
+        // membuat saldo toko
+        StoreBalance::create([
+            'store_id' => $store->id,
+            'balance' => 0,
+        ]);
+
+        // membuat user buyer
+        $buyer = User::create([
+            'name' => 'Arkadian',
+            'email' => 'arkadian@gmail.com',
+            'password' => Hash::make('buyer'),
+            'role' => 'member',
+        ]);
+
+        // membuat data buyer
+        Buyer::create([
+            'user_id' => $buyer->id,
+            'phone_number' => '085648128017',
+        ]);
+
+        // membuat kategori produk
+        $catElektronik = ProductCategory::create([
+            'name' => 'Elektronik',
+            'slug' => 'elektronik',
+            'description' => 'Komputer, laptop, handphone, dan lainnya',
+            'image' => 'elektronik.jpg'
+        ]);
+
+        $catAccessories = ProductCategory::create([
+            'name' => 'Accesssories',
+            'slug' => 'accessories',
+            'description' => 'Hedset, casing, charger, dan lainnya',
+            'image' => 'accessories.png'
+        ]);
+
+        // MEMBUAT PRODUK 
+        // produk 1
+        $product1 = Product::create([
+            'store_id' => $store->id,
+            'product_category_id' => $catElektronik->id,
+            'name' => 'Lenovo ThinkPad X1 Carbon',
+            'slug' => 'lenovo-thinkpad-x1-carbon',
+            'description' => 'Laptop bisnis.',
+            'condition' => 'new',
+            'price' => 28000000,
+            'weight' => 2500,
+            'stock' => 10,
+        ]);
+
+        // menambahkan gambar untuk produk 1
+        ProductImage::create([
+            'product_id' => $product1->id,
+            'image' => 'thinkpad.jpg',
+            'is_thumbnail' => true,
+        ]);
+
+        // produk 2
+        $product2 = Product::create([
+            'store_id' => $store->id,
+            'product_category_id' => $catAccessories->id,
+            'name' => 'Casing iPhone 12 Pro Max',
+            'slug' => 'casing-iphone-12-pro-max',
+            'description' => 'Softcase.',
+            'condition' => 'new',
+            'price' => 200000,
+            'weight' => 200,
+            'stock' => 50,
+        ]);
+
+        // menambahkan gambar untuk produk 2
+        ProductImage::create([
+            'product_id' => $product2->id,
+            'image' => 'casing.jpg',
+            'is_thumbnail' => true,
         ]);
     }
 }
