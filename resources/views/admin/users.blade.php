8632711d02f37255fe66c3e297092dc1b94b6019
Luna <inilalunaa@gmail.com>
2025-12-03 16:10:40 +0700

    menambahkan layout dan ubah views admin

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,37 +1,10 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-bold text-lunpia-dark">Manage Users</h2>
-    </x-slot>
+@extends('admin.layout')
 
-    <div class="bg-white shadow p-6 rounded-xl">
-        <h3 class="text-lg font-semibold mb-4">Users</h3>
+@section('title', 'Kelola User')
 
-        <div class="overflow-x-auto">
-            <table class="min-w-full text-sm border">
-                <thead class="bg-gray-100">
-                    <tr>
-                        <th class="p-3 text-left">Name</th>
-                        <th class="p-3 text-left">Email</th>
-                        <th class="p-3 text-left">Role</th>
-                        <th class="p-3 text-center">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $u)
-                    <tr class="border-t">
-                        <td class="p-3">{{ $u->name }}</td>
-                        <td class="p-3">{{ $u->email }}</td>
-                        <td class="p-3">{{ $u->role }}</td>
-                        <td class="p-3 text-center">
-                            <button class="px-3 py-1 rounded bg-lunpia-red text-white">
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+@section('content')
+<h2 class="text-3xl font-bold mb-6">👥 Kelola User</h2>
 
-    </div>
-</x-app-layout>
+{!! $table_html !!}
+{{-- atau paste seluruh tabel user kamu di sini --}}
+@endsection
