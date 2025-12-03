2eacfc50bc75e511d1866690d1083b6a767e8ff6
Luna <inilalunaa@gmail.com>
2025-12-03 15:32:17 +0700

    modifikasi controller admin

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
 use App\Models\User;
 
 class AdminController extends Controller
