42595b55f4e03989dbf5ae6106efe717d591a91b
Saputrawilly <naufalwillysaputra12@gmail.com>
2025-12-11 09:18:20 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,22 +1,14 @@
 <?php
-// FILE: conn.php
-
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db_name = "gmmart";
+$db_name = "UAPPEMWEB12"; // <<< GANTI NAMA DB
 
-// Buat koneksi baru
 $mysqli = new mysqli($host, $user, $pass, $db_name);
 
-// Cek koneksi
 if ($mysqli->connect_errno) {
-    // Hentikan eksekusi dan tampilkan error koneksi
     die("Koneksi database gagal: " . $mysqli->connect_error);
 }
 
-// Set karakter set ke utf8 untuk mendukung karakter khusus
 $mysqli->set_charset("utf8");
-
-// Catatan: Tidak perlu menutup koneksi di sini, biarkan tertutup di akhir skrip
 ?>
